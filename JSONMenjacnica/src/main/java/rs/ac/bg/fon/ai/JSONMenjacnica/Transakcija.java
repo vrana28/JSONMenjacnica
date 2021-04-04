f5e2d33c2c01c2430fc3119a58cd40a59f6b5930
vrana28 <vranjesdjordje@gmail.com>
2021-04-04 16:36:45 +0200

    Napravljena klasa Transkacija sa atributima izvornaValuta, krajnjaValuta, pocetniIznos, konvertovaniIznos, datumTransakcije.

---
+++ JSONMenjacnica/src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/Transakcija.java
@@ -0,0 +1,15 @@
+package rs.ac.bg.fon.ai.JSONMenjacnica;
+
+import java.util.Date;
+
+public class Transakcija {
+
+	public String izvornaValuta;
+	public String krajnjaValuta;
+	public double pocetniIznos;
+	public double konvertovaniIznos;
+	public Date datumTranskacije;
+	
+	
+	
+}
